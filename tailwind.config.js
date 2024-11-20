7e53876d401e3bd2f86559d659b8b43cf07db836
yush2428 <yush2428@gmail.com>
2024-11-20 11:50:03 +0530

    config: packages imported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff014f",
+        secondary: "#212428",
+        "card-dark": "#1e2024",
+        "text-light": "#c4cfde",
+        "text-dark": "#878e99",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      boxShadow: {
+        'card': '10px 10px 19px #1c1e22, -10px -10px 19px #262a2e',
+      },
+      backgroundImage: {
+        'card-gradient': 'linear-gradient(145deg, #1e2024, #23272b)',
+        'card-hover': 'linear-gradient(to right bottom, #212428, #16181c)',
+      },
+    },
   },
   plugins: [],
 }
-
